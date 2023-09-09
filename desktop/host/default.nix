5d033c6bda5718124dad89feb0ae45f1d4fa6411
shudawei <d-zeidler@gmx.de>
2023-09-09 08:22:00 +0200

    fix(dgb): removed hyprland nixos module

--- desktop/host/default.nix
+++ desktop/host/default.nix
@@ -3,7 +3,7 @@
 {
     imports = [ 
         ./hdw.nix 
-        pkgs-ext.hyprland.nixosModules.default
+        # pkgs-ext.hyprland.nixosModules.default
     ];
 
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
