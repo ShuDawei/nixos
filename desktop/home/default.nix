4f259b75ab7271b08f413a145991f60d4bd865cd
shudawei <d-zeidler@gmx.de>
2023-09-15 17:43:37 +0200

    feat: add socat

--- desktop/home/default.nix
+++ desktop/home/default.nix
@@ -33,6 +33,7 @@
             rust-analyzer
             lua-language-server
             pkgs-ext.nixd.packages.${pkgs.system}.nixd
+            socat
 
             hyprpicker
             pkgs-ext.hyprland-contrib.packages.${pkgs.system}.grimblast
