0cb6fa3863a109c4a950a0a70676bd9805effe75
shudawei <d-zeidler@gmx.de>
2023-09-15 11:47:20 +0200

    feat: firefox userChrome.css

--- desktop/home/cfg/firefox.nix
+++ desktop/home/cfg/firefox.nix
@@ -1,5 +1,16 @@
 { ... }:
 
 {
-    programs.firefox.enable = true;
+    programs.firefox = {
+        enable = true;
+        profiles.default.userChrome = ''
+            /* Hide Tab bar with only one Tab - [110] */
+            #tabbrowser-tabs .tabbrowser-tab:only-of-type,
+            #tabbrowser-tabs .tabbrowser-tab:only-of-type + #tabbrowser-arrowscrollbox-periphery{
+                display:none !important;
+            }
+            #tabbrowser-tabs, #tabbrowser-arrowscrollbox {min-height:0!important;}
+            /* #TabsToolbar:not(:hover) */ #alltabs-button {display:none !important;}
+        '';
+    };
 }
