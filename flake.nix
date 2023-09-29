5d2491df81f3aad744b7175ae2d22379a74a7a22
shudawei <d-zeidler@gmx.de>
2023-09-29 11:18:06 +0200

    fix: remove getchoo input, remove modrinth app

--- flake.nix
+++ flake.nix
@@ -23,10 +23,6 @@
       url = "github:nix-community/nixd";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    getchoo = {
-        url = "github:getchoo/nix-exprs";
-        inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
@@ -40,7 +36,7 @@
       config.allowUnfree = true;
     };
     pkgs-ext = {
-      inherit (inputs) home-manager neovim hyprland hyprland-contrib nixd getchoo;
+      inherit (inputs) home-manager neovim hyprland hyprland-contrib nixd;
     };
   in {
     nixosConfigurations = {
