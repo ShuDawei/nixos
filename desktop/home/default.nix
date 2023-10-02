f0018f15cc07f7aa025db276e7b15a14ac7bd42b
shudawei <d-zeidler@gmx.de>
2023-10-02 10:19:45 +0200

    feat: add xwaylandvideobridge (getcho/nix-exprs)

--- desktop/home/default.nix
+++ desktop/home/default.nix
@@ -27,6 +27,7 @@
     packages = with pkgs; [
       (import ./scripts/vol.nix pkgs)
       pkgs-ext.neovim.packages.${pkgs.system}.neovim
+      pkgs-ext.getchoo.packages.${pkgs.system}.xwaylandvideobridge
 
       rustc
       cargo
