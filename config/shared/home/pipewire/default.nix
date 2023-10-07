234a164b7a701b41b4fecb87dae5a7ece348c43a
shudawei <d-zeidler@gmx.de>
2023-10-07 11:57:11 +0200

    dbg

--- config/shared/home/pipewire/default.nix
+++ config/shared/home/pipewire/default.nix
@@ -1,6 +1,4 @@
-{...}: {
-  home.file.".config/pipewire" = {
-    source = ./pipewire;
-    recursive = true;
-  };
+{pkgs, ...}: {
+  home.file.".config/pipewire/pipewire.conf".source = ./pipewire.conf;
+  home.file.".config/pipewire/pipewire.conf.d/normalizer.conf".source = ./normalizer.conf.nix pkgs;
 }
