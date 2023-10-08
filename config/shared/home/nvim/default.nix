930888ccbead575d915067aeb41554288b81761e
shudawei <d-zeidler@gmx.de>
2023-10-08 11:03:04 +0200

    feat: migrate to home manager for nvim plugins

--- config/shared/home/nvim/default.nix
+++ config/shared/home/nvim/default.nix
@@ -1,8 +1,25 @@
 {pkgs, ...}: {
   programs.neovim = {
     enable = true;
-    plugins = with pkgs; [
-      (vimPlugins.nvim-treesitter.withPlugins (p: []))
+    plugins = with pkgs.vimPlugins; [
+      cmp-buffer
+      cmp-nvim-lsp
+      cmp-path
+      fidget-nvim
+      gitsigns-nvim
+      lualine-nvim
+      luasnip
+      neodev-nvim
+      nvim-autopairs
+      nvim-cmp
+      nvim-colorizer-lua
+      nvim-lspconfig
+      nvim-treesitter.withAllGrammars
+      oil-nvim
+      presence-nvim
+      rose-pine
+      telescope-nvim
+      vim-fugitive
     ];
   };
 }
