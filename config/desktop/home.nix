b046452d5dd82472bc334b1a306ea901b2039068
shudawei <d-zeidler@gmx.de>
2023-10-10 10:54:41 +0200

    dbg

--- config/desktop/home.nix
+++ config/desktop/home.nix
@@ -30,7 +30,7 @@
       ladspaPlugins
       (import ../shared/scripts/vol.nix pkgs)
       #pkgs-ext.neovim.packages.${pkgs.system}.neovim
-      ../shared/drv/xwaylandvideobridge.nix
+      (callPackage ../shared/drv/xwaylandvideobridge.nix { })
 
       rustc
       cargo
