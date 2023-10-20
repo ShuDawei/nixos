c3704492649bdc4acbd502f19cf626e19f3f2915
shudawei <d-zeidler@gmx.de>
2023-10-20 13:31:28 +0200

    dbg

--- dbg/default.nix
+++ dbg/default.nix
@@ -4,5 +4,6 @@ pkgs.rustPlatform.buildRustPackage rec {
     pname = "dbg";
     version = "0.5.4";
     src = ./.;
-    cargoLock. lockFile = "${src}/Cargo.lock";
+    cargoLock.lockFile = "${src}/Cargo.lock";
+    testAndBuildSubDir = "sub";
 }
