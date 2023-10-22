4b4920ff239e42fed4b51e19a18157be09ae611d
shudawei <d-zeidler@gmx.de>
2023-10-22 11:07:54 +0200

    dbg

--- config/shared/drv/theseus/default.nix
+++ config/shared/drv/theseus/default.nix
@@ -3,15 +3,15 @@ pkgs:
 pkgs.rustPlatform.buildRustPackage rec {
     pname = "theseus_cli";
     version = "0.5.4";
-    src = ./.;
-    #src = pkgs.fetchFromGitHub {
-    #    owner = "modrinth";
-    #    repo = "theseus";
-    #    rev = "1e8852b54007f8739d94c4593aa369ac512b30fc";
-    #    sha256 = "1zykw5wnn3ilqxxahrggrxzzrgg1265338kxvhl8s7h7wicp7gll";
-    #};
+    #src = ./.;
+    src = pkgs.fetchFromGitHub {
+        owner = "modrinth";
+        repo = "theseus";
+        rev = "afaec4b1bf04a06070d3087baae30ba4673f7f4b";
+        sha256 = "0blxpa2qhpmjr7xa7vi7wnjk842cj1286pn90gnak98rz06ax76d";
+    };
     cargoLock = {
-        lockFile = ./Cargo.lock;
+        lockFile = "${src}/Cargo.lock";
         outputHashes = {
             "tauri-plugin-single-instance-0.0.0" = "sha256-G4h2OXKPpZMmradutdUWxGG5axL9XMz2ACAe8AQ40eg=";
         };
