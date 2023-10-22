8f213b514ea4e33fbc58ccf38774a5160016c5e7
shudawei <d-zeidler@gmx.de>
2023-10-22 14:29:42 +0200

    dbg

--- config/desktop/host.nix
+++ config/desktop/host.nix
@@ -12,7 +12,7 @@
 
   nix.settings = {
     experimental-features = ["nix-command" "flakes"];
-    trusted-substituters = ["https://cache.garnix.io"];
+    substituters = ["https://cache.nixos.org" "https://cache.garnix.io"];
     trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
   };
 
