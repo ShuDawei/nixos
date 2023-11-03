1e8e81c172717632f55f8eb773852e44880717d6
shudawei <d-zeidler@gmx.de>
2023-11-03 10:54:01 +0100

    fix: actually install grimblast

--- common/home/hyprland/default.nix
+++ common/home/hyprland/default.nix
@@ -3,8 +3,7 @@
 {
   home.packages = with pkgs; [
     hyprpaper
-    slurp
-    jq
+    inputs.hyprwm-contrib.packages.${system}.grimblast
   ];
   home.file.".config/hypr/hyprpaper.conf".text = ''
     splash = true;
@@ -62,7 +61,7 @@
         "SUPER SHIFT,8,movetoworkspace,8"
         "SUPER SHIFT,9,movetoworkspace,9"
         "SUPER SHIFT,0,movetoworkspace,10"
-        ",PRINT,exec,${inputs.hyprwm-contrib.packages.${system}.grimblast}/grimblast --freeze copy area"
+        ",PRINT,exec,grimblast --freeze copy area"
         ",KP_ADD,exec,vol mic"
         ",KP_SUBTRACT,pass,^(com.obsproject.Studio)$"
       ];
