508c5a3396995de7aa583d89c8c2ccc986e89c2a
shudawei <d-zeidler@gmx.de>
2024-06-05 10:27:34 +0200

    fix: remove presence.nvim

--- common/home/nvim/nvim/lua/03-misc.lua
+++ common/home/nvim/nvim/lua/03-misc.lua
@@ -10,9 +10,6 @@ vim.keymap.set("n", "<leader>fe", ":Oil<cr>")
 -- windwp/nvim-autopairs --
 require("nvim-autopairs").setup()
 
--- andweeb/presence.nvim --
-require("presence").setup()
-
 -- stevearc/dressing.nvim --
 require("dressing").setup()
 
