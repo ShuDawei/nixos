15b4b0bdc9859c59c785d0ce7b269ae33d1dd0b3
shudawei <d-zeidler@gmx.de>
2024-06-07 12:05:13 +0200

    fix: add some nvidia settings

--- desktop/host.nix
+++ desktop/host.nix
@@ -6,5 +6,11 @@
     modesetting.enable = true;
     powerManagement.enable = true;
     nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable
   };
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  }
 }
