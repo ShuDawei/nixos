f4c6348ba18e591882ea1d1ea142227f7749f1ff
shudawei <d-zeidler@gmx.de>
2024-06-29 09:48:51 +0200

    tmp

--- common/home/nvim/nvim/lua/03-misc.lua
+++ common/home/nvim/nvim/lua/03-misc.lua
@@ -36,3 +36,10 @@ require("nvim-treesitter.configs").setup({
     highlight = { enable = true },
     indent = { enable = true },
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        client.server_capabilities.semanticTokensProvider = nil
+    end,
+});
