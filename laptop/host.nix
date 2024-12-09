1ce26dfc3544896c03bcd1fb53ddfdc93b3339aa
shudawei <destocksiefen@gmail.com>
2024-12-09 13:15:34 +0100

    feat: add auto-cpufreq

--- laptop/host.nix
+++ laptop/host.nix
@@ -5,5 +5,17 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   powerManagement.enable = true;
-services.thermald.enable = true;
+  services.thermald.enable = true;
+  
+services.auto-cpufreq.enable = true;
+services.auto-cpufreq.settings = {
+  battery = {
+     governor = "powersave";
+     turbo = "never";
+  };
+  charger = {
+     governor = "performance";
+     turbo = "auto";
+  };
+};
 }
