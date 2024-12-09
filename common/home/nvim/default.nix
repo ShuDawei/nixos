c8db6b51ecec75eded3b36d77f1ce41d60a911b5
shudawei <destocksiefen@gmail.com>
2024-12-09 16:14:05 +0100

    fix: remove treesitter nu

--- common/home/nvim/default.nix
+++ common/home/nvim/default.nix
@@ -17,7 +17,8 @@
       nvim-colorizer-lua
       nvim-jdtls
       nvim-lspconfig
-      (nvim-treesitter.withPlugins (_: nvim-treesitter.allGrammars ++ [ pkgs.tree-sitter.builtGrammars.tree-sitter-nu ]))
+      (nvim-treesitter.withPlugins (_: nvim-treesitter.allGrammars)
+      #(nvim-treesitter.withPlugins (_: nvim-treesitter.allGrammars ++ [ pkgs.tree-sitter.builtGrammars.tree-sitter-nu ]))
       oil-nvim
       rose-pine
       telescope-nvim
