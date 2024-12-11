1a32073ed72f4022ebc25761b6de3802dc18389c
shudawei <destocksiefen@gmail.com>
2024-12-11 09:54:53 +0100

    fix: comment out line

--- laptop/host.nix
+++ laptop/host.nix
@@ -25,17 +25,17 @@
     };
   };
 
-    #services.xserver.videoDrivers = [ "nvidia" ];
-    #hardware.nvidia = {
-    #  package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
-    #  modesetting.enable = true;
-    #  powerManagement.enable = true;
-    #prime = {
-    #  sync.enable = true;
-    #  intelBusId = "PCI:0:2:0";
-    #  nvidiaBusId = "PCI:1:0:0";
-    #};
-  };
+  #services.xserver.videoDrivers = [ "nvidia" ];
+  #hardware.nvidia = {
+  #  package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+  #  modesetting.enable = true;
+  #  powerManagement.enable = true;
+  #prime = {
+  #  sync.enable = true;
+  #  intelBusId = "PCI:0:2:0";
+  #  nvidiaBusId = "PCI:1:0:0";
+  #};
+  #};
 
   environment.systemPackages = [ inputs.nixpkgs-stable.legacyPackages.${system}.libva-utils ];
   hardware.graphics.extraPackages = [
