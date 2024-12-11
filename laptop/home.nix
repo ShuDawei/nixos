fd79b5e834ad8bfa8d664712bdc80d968953e6f3
shudawei <destocksiefen@gmail.com>
2024-12-11 15:41:31 +0100

    test: hyprland monitor auto scale

--- laptop/home.nix
+++ laptop/home.nix
@@ -4,7 +4,7 @@
   wayland.windowManager.hyprland = {
     settings = {
       monitor = [
-        "LVDS-1,1280x720@60,0x0,1"
+        "LVDS-1,1280x720@60,0x0,auto"
         "LVDS-2,1280x720@60,0x0,1"
       ];
     };
