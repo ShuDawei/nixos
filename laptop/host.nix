718efbfe195f1cb6d0f09ba0c2f77380f0eb7765
shudawei <destocksiefen@gmail.com>
2024-12-11 10:11:50 +0100

    test

--- laptop/host.nix
+++ laptop/host.nix
@@ -41,7 +41,7 @@
   hardware.graphics.extraPackages = [
     #pkgs.intel-vaapi-driver
     inputs.nixpkgs-stable.legacyPackages.${system}.intel-vaapi-driver
-    inputs.nixpkgs-stable.legacyPackages.${system}.ibvdpau-va-gl
+    pkgs.ibvdpau-va-gl
   ];
   environment.sessionVariables = {
     LIBVA_DRIVER_NAME = "i965";
