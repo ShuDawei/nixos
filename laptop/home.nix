033c79fed3cb074a4c6debc56fed534d5984ca55
shudawei <destocksiefen@gmail.com>
2024-12-11 15:45:01 +0100

    fix: revert back to 1080p

--- laptop/home.nix
+++ laptop/home.nix
@@ -4,8 +4,8 @@
   wayland.windowManager.hyprland = {
     settings = {
       monitor = [
-        "LVDS-1,1280x720@60,0x0,auto"
-        "LVDS-2,1280x720@60,0x0,1"
+        "LVDS-1,1920x1080@60,0x0,1"
+        "LVDS-2,1920x1080@60,0x0,1"
       ];
     };
   };
