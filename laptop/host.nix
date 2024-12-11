74809081ca46a1206ea15b646808f47ef2556853
shudawei <destocksiefen@gmail.com>
2024-12-11 11:47:04 +0100

    fix: revert to intel

--- laptop/host.nix
+++ laptop/host.nix
@@ -26,34 +26,34 @@
   };
 
   #services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
-    modesetting.enable = true;
-    powerManagement.enable = true;
-    #prime = {
-    #  sync.enable = true;
-    #  intelBusId = "PCI:0:2:0";
-    #  nvidiaBusId = "PCI:1:0:0";
-    #};
-  };
+  #hardware.nvidia = {
+  #  package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+  #  modesetting.enable = true;
+  #  powerManagement.enable = true;
+  #  #prime = {
+  #  #  sync.enable = true;
+  #  #  intelBusId = "PCI:0:2:0";
+  #  #  nvidiaBusId = "PCI:1:0:0";
+  #  #};
+  #};
 
-  environment.systemPackages = [ pkgs.libva-utils ];
-  hardware.graphics.extraPackages = [
-    pkgs.vaapiVdpau
-    pkgs.libvdpau-va-gl
-    pkgs.nvidia-vaapi-driver
-  ];
-  environment.sessionVariables = {
-    LIBVA_DRIVER_NAME = "nvidia";
-  };
-
-  #environment.systemPackages = [ inputs.nixpkgs-stable.legacyPackages.${system}.libva-utils ];
+  #environment.systemPackages = [ pkgs.libva-utils ];
   #hardware.graphics.extraPackages = [
-  #  #pkgs.intel-vaapi-driver
-  #  inputs.nixpkgs-stable.legacyPackages.${system}.intel-vaapi-driver
-  #  #inputs.nixpkgs-stable.legacyPackages.${system}.libvdpau-va-gl
+  #  pkgs.vaapiVdpau
+  #  pkgs.libvdpau-va-gl
+  #  pkgs.nvidia-vaapi-driver
   #];
   #environment.sessionVariables = {
-  #  LIBVA_DRIVER_NAME = "i965";
+  #  LIBVA_DRIVER_NAME = "nvidia";
   #};
+
+  environment.systemPackages = [ inputs.nixpkgs-stable.legacyPackages.${system}.libva-utils ];
+  hardware.graphics.extraPackages = [
+    #pkgs.intel-vaapi-driver
+    inputs.nixpkgs-stable.legacyPackages.${system}.intel-vaapi-driver
+    #inputs.nixpkgs-stable.legacyPackages.${system}.libvdpau-va-gl
+  ];
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "i965";
+  };
 }
