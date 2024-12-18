b543687246c57c8c20eb48f06938c34af58fc2df
shudawei <destocksiefen@gmail.com>
2024-12-18 11:36:28 +0100

    fix: fix monitor scaling in hyprland

--- p50/home.nix
+++ p50/home.nix
@@ -1,12 +1,13 @@
 { ... }:
 
 {
-  #wayland.windowManager.hyprland = {
-  #  settings = {
-  #    monitor = [
-  #      "LVDS-1,1920x1080@60,0x0,1"
-  #      "LVDS-2,1920x1080@60,0x0,1"
-  #    ];
-  #  };
-  #};
+  wayland.windowManager.hyprland = {
+    settings = {
+      monitor = [
+        "eDP-1,1920x1080@60,0x0,1"
+        #"LVDS-1,1920x1080@60,0x0,1"
+        #"LVDS-2,1920x1080@60,0x0,1"
+      ];
+    };
+  };
 }
