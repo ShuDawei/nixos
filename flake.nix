ccf6249502415a09e681afcb5f07e6a6814d46d3
shudawei <destocksiefen@gmail.com>
2024-12-18 12:34:15 +0100

    fix: formatting

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
       nixosConfigurations = {
         desktop = util.nixosConfiguration "shudawei" "desktop";
         laptop = util.nixosConfiguration "shudawei" "laptop";
-	p50 = util.nixosConfiguration "shudawei" "p50";
+        p50 = util.nixosConfiguration "shudawei" "p50";
       };
     };
 }
