8b6ff64d0db4b689a17d5841cc7f2f727983746d
shudawei <d-zeidler@gmx.de>
2024-12-20 15:04:22 +0100

    test

--- desktop/home.nix
+++ desktop/home.nix
@@ -5,7 +5,7 @@
     settings = {
       env = [ "NIXOS_OZONE_WL,1" ];
       monitor = [
-        "DP-3,1920x1080@240,0x0,1,bitdepth,10"
+        "DP-3,1920x1080@240,0x0,1,bitdepth,8"
         "Unknown-1,disable"
       ];
     };
